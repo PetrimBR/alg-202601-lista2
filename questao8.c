1c803aa0ce55c75cfd32292d5012a5c8fd4f7223
Petrim <147660700+PetrimBR@users.noreply.github.com>
2026-04-20 14:38:30 -0300

    Create questao8.c

---
+++ questao8.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    float peso, altura, imc;
+
+    printf("=== Calculadora de IMC ===\n");
+    printf("Digite seu peso (kg): ");
+    scanf("%f", &peso);
+    printf("Digite sua altura (m): ");
+    scanf("%f", &altura);
+  
+    imc = peso / (altura * altura);
+
+    printf("\nSeu IMC: %.2f\n", imc);
+
+    if (imc < 18.5) {
+        printf("Classificacao: Abaixo do peso\n");
+    } else if (imc < 25.0) {
+        printf("Classificacao: Peso normal\n");
+    } else if (imc < 30.0) {
+        printf("Classificacao: Sobrepeso\n");
+    } else if (imc < 35.0) {
+        printf("Classificacao: Obesidade grau I\n");
+    } else if (imc < 40.0) {
+        printf("Classificacao: Obesidade grau II\n");
+    } else {
+        printf("Classificacao: Obesidade grau III\n");
+    }
+
+    return 0;
+}
