b4f8aeee4dbe0d737cd52825292b2f7f0ce6fc37
Petrim <147660700+PetrimBR@users.noreply.github.com>
2026-04-20 17:31:07 -0300

    Improve input validation and vowel/consonant check
    Refactor the main function to validate input and check for vowels or consonants.

--- questao3.c
+++ questao3.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <ctype.h>
 
-int main(){
-
+int main() {
     char letra;
-    
-    printf("digite uma letra: ");
-    scanf("%c", &letra);
-    
-    if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u'){
-    printf("isso e uma vogal\n");
-}
-    else {
-    printf("isso e uma consoante\n");
-}
 
-    system("pause");
+    printf("Digite uma letra: ");
+    scanf(" %c", &letra);
+    
+    letra = tolower(letra);
+
+    if (letra < 'a' || letra > 'z') {
+        printf("Caractere invalido! Digite apenas letras.\n");
+    } else if (letra == 'a' || letra == 'e' || letra == 'i' ||
+               letra == 'o' || letra == 'u') {
+        printf("'%c' e uma VOGAL.\n", letra);
+    } else {
+        printf("'%c' e uma CONSOANTE.\n", letra);
+    }
+
     return 0;
 }
