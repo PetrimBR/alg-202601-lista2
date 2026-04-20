990b1a06b28deacbb444d2d28e8ea89b6caaadfe
Petrim <147660700+PetrimBR@users.noreply.github.com>
2026-04-20 14:35:17 -0300

    Create questao6.c

---
+++ questao6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int i, j, soma = 0;
+
+    printf("Pecas do domino com o numero 4:\n");
+
+    for (i = 0; i <= 6; i++) {
+        for (j = i; j <= 6; j++) {
+            if (i == 4 || j == 4) {
+                printf("  [%d|%d] = %d\n", i, j, i + j);
+                soma = soma + i + j;
+            }
+        }
+    }
+
+    printf("Soma total dos 4s no domino: %d\n", soma);
+
+    return 0;
+}
