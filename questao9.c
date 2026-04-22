9248008f00ec66288194f982a939e25a5de4a564
Pedro Torres <147660700+PetrimBR@users.noreply.github.com>
2026-04-22 18:36:24 -0300

    Update questao9.c

--- questao9.c
+++ questao9.c
@@ -5,6 +5,9 @@ int main() {
     float tamanho_mb, velocidade_mbps;
     float tamanho_mbits, tempo_seg, tempo_min;
 
+    int min_inteiros;
+    float segundos_restantes;
+
     printf("=== Calculadora de Tempo de Download ===\n");
     printf("Tamanho do arquivo (MB): ");
     scanf("%f", &tamanho_mb);
@@ -12,15 +15,17 @@ int main() {
     scanf("%f", &velocidade_mbps);
 
     tamanho_mbits = tamanho_mb * 8;
-
     tempo_seg = tamanho_mbits / velocidade_mbps;
-
     tempo_min = tempo_seg / 60;
 
+    min_inteiros = (int)tempo_min; 
+
+    segundos_restantes = (tempo_min - min_inteiros) * 60;
+
     printf("\nTempo aproximado de download:\n");
-    printf("  %.2f segundos\n", tempo_seg);
-    printf("  %.2f minutos\n", tempo_min);	
-    
-	system("pause");
+    printf("  Total em segundos: %.2f\n", tempo_seg);
+    printf("  Tempo formatado:   %d min e %.0f segundos\n", min_inteiros, segundos_restantes);	
+
+    system("pause");
     return 0;
 }
