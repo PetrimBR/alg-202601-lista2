c71aee34609fa16881699a82630c649744ed96cf
Pedro Torres <147660700+PetrimBR@users.noreply.github.com>
2026-04-22 17:58:55 -0300

    Update questao6.c

--- questao6.c
+++ questao6.c
@@ -2,20 +2,21 @@
 #include <stdlib.h>
 
 int main() {
-    int i, j, soma = 0;
+	int i, j, soma = 0;
 
-    printf("Pecas do domino com o numero 4:\n");
-
-    for (i = 0; i <= 6; i++) {
-        for (j = i; j <= 6; j++) {
-            if (i == 4 || j == 4) {
-                printf("  [%d|%d] = %d\n", i, j, i + j);
-                soma = soma + i + j;
-            }
-        }
-    }
-
-    printf("Soma total dos 4s no domino: %d\n", soma);
-
-    return 0;
+	printf("Pecas do domino com o numero 4:\n");
+	
+	for (i = 0; i <= 6; i++) {
+	    for (j = i; j <= 6; j++) {
+	        if (i == 4 || j == 4) {
+	            printf("  [%d|%d] = %d\n", i, j, i + j);
+	            soma = soma + i + j;
+	        }
+	    }
+	}
+	
+	printf("Soma total dos 4s no domino: %d\n", soma);
+	    
+	system("pause");
+	return 0;	
 }
